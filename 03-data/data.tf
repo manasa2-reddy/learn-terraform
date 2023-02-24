0b0e1cc28340ee35897cc392100715b9580f7dd5
manasa <manasa.busireddy@gmail.com>
2023-02-24 13:01:35 -0500

    test terraform

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "centos-8-DevOps-practice"
-  owners           = ["97371447688"]
+  owners           = ["973714476881"]
   }
 
 output "ami_id" {
