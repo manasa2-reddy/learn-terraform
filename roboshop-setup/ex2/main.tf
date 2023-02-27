8fa37443a9c2b85ad53d34221943d8197029158a
manasa <manasa.busireddy@gmail.com>
2023-02-27 15:07:45 -0500

    test1

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -5,8 +5,12 @@ data "aws_ami" "ami" {
 }
 
 resource "aws_instance" "frontend" {
-  count                  = 5
+  count                  = length(var.instances)
   ami                    = data.aws_ami.ami.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-094ac9e937067c392"]
   }
+
+variable "instances" {
+  default = ["cart", "catalogue", "user", "payment", "Shipping"]
+}
