571fd751a58935833e093be579f328b577d7c238
manasa <manasa.busireddy@gmail.com>
2023-02-27 13:53:59 -0500

    test1

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,13 +1,13 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex = "Centos-8-DevOps-practice"
-  owners = ["973714476881"]
+  name_regex  = "Centos-8-DevOps-practice"
+  owners      = ["973714476881"]
 }
 
 resource "aws_instance" "frontend" {
-  count= 5
-  ami = "ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
+  count                  = 5
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-094ac9e937067c392"]
 
   }
