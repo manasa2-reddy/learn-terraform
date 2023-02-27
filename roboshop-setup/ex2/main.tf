73ba600935e500a6b1a69d64d10ccdbb1c3534b1
manasa <manasa.busireddy@gmail.com>
2023-02-27 13:56:03 -0500

    test1

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -9,5 +9,4 @@ resource "aws_instance" "frontend" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-094ac9e937067c392"]
-
   }
