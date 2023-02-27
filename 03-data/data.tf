05952c2b9934c0f839ad5737f7b56c0038514fcb
manasa <manasa.busireddy@gmail.com>
2023-02-27 14:55:09 -0500

    test1

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -2,8 +2,8 @@ data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "centos-8-DevOps-practice"
   owners           = ["973714476881"]
-  }
+}
 
 output "ami_id" {
-  value = data.aws_ami.ami.image_id
+  value = data.aws_ami.ami["image_id"]
 }
