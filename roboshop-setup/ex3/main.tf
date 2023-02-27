228c242099db0ac940c860b52c3c4fc5207dfd63
manasa <manasa.busireddy@gmail.com>
2023-02-27 17:28:59 -0500

    test1

--- roboshop-setup/ex3/main.tf
+++ roboshop-setup/ex3/main.tf
@@ -4,7 +4,7 @@ data "aws_ami" "ami" {
   owners      = ["973714476881"]
 }
 
-resource "aws_instance" "instances" {
+/*resource "aws_instance" "instances" {
  for_each = var.instances
   ami                    = data.aws_ami.ami.image_id
   instance_type          = each.value["type"]
@@ -26,4 +26,45 @@ variable "instances" {
       type = "t3.small"
     }
   }
+}*/
+
+/*variable "names" {
+  default = [ "catalogue","user"]
+}
+variable "type" {
+  default = ["t3.micro,t3.small"]
+}
+
+resource "aws_instance" "instances" {
+ count = length(var.names)
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.type[count.index]
+  vpc_security_group_ids = ["sg-094ac9e937067c392"]
+  tags                   = {
+    Name = var.names[count.index]
+  }
+}*/
+
+/*little mature code*/
+variable "demo" {
+  default = [
+     {
+      name = "catalogue"
+      type = "t3.micro"
+    },
+     {
+      name = "user"
+      type = "t3.small"
+    }
+  ]
+}
+
+resource "aws_instance" "instances" {
+count                  = length(var.demo)
+ami                    = data.aws_ami.ami.image_id
+instance_type          = var.demo[count.index]["type"]
+vpc_security_group_ids = ["sg-094ac9e937067c392"]
+tags                   = {
+Name = var.demo[count.index]["name"]
+}
 }
