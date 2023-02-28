1d8e1576cd4fc0f4daa200d027f263f31fc688af
manasa <manasa.busireddy@gmail.com>
2023-02-28 18:59:44 -0500

    test12

--- roboshop-setup/ex4/main.tf
+++ roboshop-setup/ex4/main.tf
@@ -3,7 +3,6 @@ module "ec2" {
   source = "./ec2"
  component = each.value["name"]
   instance_type = each.value["type"]
-  sg_id        = module.sg.sg_id
 }
 
 module "sg" {
@@ -14,7 +13,7 @@ variable "instances" {
   default = {
     catalogue = {
       name = "catalogue"
-      type = "t3.micro"
+      type = "t3.micro"                
     }
     user = {
       name = "user"
