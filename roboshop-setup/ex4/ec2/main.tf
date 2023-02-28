f9b0bf19b76e2526d82baeac5ebd045fd7f46f05
manasa <manasa.busireddy@gmail.com>
2023-02-28 17:07:31 -0500

    test1

--- roboshop-setup/ex4/ec2/main.tf
+++ roboshop-setup/ex4/ec2/main.tf
@@ -5,7 +5,7 @@ data "aws_ami" "ami" {
 }
 
 resource "aws_instance" "ec2" {
-  ami = "ami-0a017d8ceb274537d"
+  ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = ["sg-094ac9e937067c392"]
   tags = {
@@ -23,3 +23,4 @@ resource "aws_route53_record" "record" {
 
 variable "component" {}
 variable "instance_type" {}
+variable "sg_id" {}
