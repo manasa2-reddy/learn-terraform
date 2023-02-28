97a31310406507efc39c144446b10f0184b8a0d3
manasa <manasa.busireddy@gmail.com>
2023-02-28 15:38:18 -0500

    test1

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -1,16 +1,16 @@
 resource "aws_instance" "test" {
    ami = "ami-0a017d8ceb274537d"
-  instance_type = var.instance_type =="" ? "t3.micro" : var.instance_type
+  instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
 
 }
 
 variable "instance_type" {}
 
-variable "instance" {}
+variable "create_instance" {}
 
 resource "aws_instance" "test1" {
   count = var.create_instance == "true" ? 1 : 0
-  ami =   "ami-0a017d8ceb274537d"
+  ami   =   "ami-0a017d8ceb274537d"
   instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
   
 }
