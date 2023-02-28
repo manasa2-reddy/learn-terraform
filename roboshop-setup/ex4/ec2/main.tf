47ece7917830e933b8614c2e97e3d705180a0f76
manasa <manasa.busireddy@gmail.com>
2023-02-28 16:21:32 -0500

    test1

---
+++ roboshop-setup/ex4/ec2/main.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = var.instance_type
+  vpc_security_group_ids = ["sg-094ac9e937067c392"]
+  tags = {
+    name = var.component
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z075358629IBJXB7AQLTD"
+  name    ="${var.component}.dev.devopsb71.store"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
