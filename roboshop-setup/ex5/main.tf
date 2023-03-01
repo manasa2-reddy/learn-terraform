a675293cf57f35cd7905d6d853ad2667c9844c86
manasa <manasa.busireddy@gmail.com>
2023-03-01 09:53:47 -0500

    test12

--- roboshop-setup/ex5/main.tf
+++ roboshop-setup/ex5/main.tf
@@ -9,6 +9,10 @@ module "ec2" {
   instance_type = each.value["type"]
   sg_id    = module.sg.sg_id
 }
+module "route53" {
+  source = "./route53"
+  private_ip = module.ec2.private_ip
+}
 
   
 output "ec2" {
