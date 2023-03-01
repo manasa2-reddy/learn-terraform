d155214516b8124ce89d598bf85854b9fa19dd71
manasa <manasa.busireddy@gmail.com>
2023-02-28 20:29:37 -0500

    test12

--- roboshop-setup/ex5/main.tf
+++ roboshop-setup/ex5/main.tf
@@ -1,13 +1,30 @@
-module "sg" {
-  source = "./sg"
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
 
-module "ec2" {
-  for_each = var.instances
-  source = "./ec2"
-  component = each.value["name"]
-  instance_type = each.value["type"]
-  sg_id    = module.sg.sg_id
+output "sg_id" {
+  value = aws_security_group.allow_tls.id
 }
 
 
